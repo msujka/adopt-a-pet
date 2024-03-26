b90b7c4451143251591da6c8a5791ccd170ab225
Michal Sujka <msujka@Michals-MacBook-Air.local>
2024-03-26 22:05:27 +0000

    init

---
+++ src/components/search/index.js
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react';
+// Import createSearchParams
+import { createSearchParams } from 'react-router-dom';
+// Import useNavigate
+import { useNavigate } from 'react-router-dom';
+
+const Search = () => {
+
+  // get navigate function
+  const navigate = useNavigate();
+
+  const searchInputRef = useRef();
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+
+    const searchQuery = {
+      name: searchInputRef.current.value
+    }
+
+    const query = createSearchParams(searchQuery);
+
+    // imperatively redirect with useNavigate() returned function
+    navigate({
+      pathname: '/search',
+      search: `?${query}`
+    });
+  };
+
+  return (
+    <form onSubmit={onSearchHandler} className="search-form">
+      <input type="text" className="search" ref={searchInputRef} />
+      <button type="submit" className="search-button">
+        🔎
+      </button>
+    </form>
+  );
+};
+
+export default Search;
